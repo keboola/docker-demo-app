d0bacf8b5ab99f8d570d46b61123ed3fde7d0391
Odin <odinuv@gmail.com>
2016-07-28 08:37:21 +0200

    fix: remove deprecated option

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/docker-demo-app quay.io/keboola/docker-demo-app:$TRAVIS_TAG
 docker tag keboola/docker-demo-app quay.io/keboola/docker-demo-app:latest
 docker tag keboola/docker-demo-app quay.io/keboola/docker-demo-private:$TRAVIS_TAG
