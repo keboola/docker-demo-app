a361e15077f747f3897a81e1516a68e96e740c65
Odin <odinuv@gmail.com>
2016-07-31 12:28:17 +0200

    fix: cleanup

--- tests/run.sh
+++ tests/run.sh
@@ -2,7 +2,7 @@
 echo "Starting tests" >&1
 php --version \
     && composer --version \
-    && composer install --dev \
+    && composer install \
     && /code/vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
     && /code/vendor/bin/phpunit --coverage-clover build/logs/clover.xml \
     && /code/vendor/bin/test-reporter
