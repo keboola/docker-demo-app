0c2af62d4c7b36e31129be302fe53ffaf9604e10
Odin <odinuv@gmail.com>
2016-08-01 09:34:47 +0200

    chore: deploy to dockerhub too

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,10 @@ docker push quay.io/keboola/docker-demo-app:$TRAVIS_TAG
 docker push quay.io/keboola/docker-demo-app:latest
 docker push quay.io/keboola/docker-demo-private:$TRAVIS_TAG
 docker push quay.io/keboola/docker-demo-private:latest
+
+docker login -u="$DOCKERHUB_USERNAME" -p="$DOCKERHUB_PASSWORD" index.docker.io
+docker tag keboola/docker-demo-app keboolaprivatetest/docker-demo-docker:$TRAVIS_TAG
+docker tag keboola/docker-demo-app keboolaprivatetest/docker-demo-docker:latest
+docker images
+docker push keboolaprivatetest/docker-demo-docker:$TRAVIS_TAG
+docker push keboolaprivatetest/docker-demo-docker:latest
