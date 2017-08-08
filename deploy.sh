e1333622cc0c4610f8cdc03e5b13525e090f84dc
Odin <odinuv@gmail.com>
2017-08-08 10:35:57 +0200

    turn error mode on

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag $KBC_APP_REPOSITORY quay.io/$KBC_APP_REPOSITORY:$TRAVIS_TAG
 docker tag $KBC_APP_REPOSITORY quay.io/$KBC_APP_REPOSITORY:latest
