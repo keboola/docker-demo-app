7c16360bf54e8ae86361a3e8c00d2d1faf50125e
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-16 17:17:36 +0100

    fix: remove tests/run.sh

--- tests/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting tests" >&1
-php --version \
-    && composer --version \
-    && composer install \
-    && /code/vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
-    && /code/vendor/bin/phpstan analyse --level=7 ./src ./tests \
-    && /code/vendor/bin/phpunit --coverage-clover build/logs/clover.xml \
-    && /code/vendor/bin/test-reporter
